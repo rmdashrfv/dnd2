a90ef434f56c6da4f6abc7de051e2608effd0758
Michael Law <rmdashrfv@hey.com>
2022-07-18 17:14:35 -0400

    video background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import './App.css'
 
 function App() {
   // This syntax is called JSX = JavaScript XML
@@ -8,7 +8,16 @@ function App() {
   // to update it
   
   return (
-    <div>
+    <div className="screen">
+      <video autoPlay muted loop id="myVideo">
+        <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mov" />
+      </video>
+      <div className="" id="fs-overlay">
+      <img
+        id="header-img"
+        src='https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png'
+      />
+      </div>
     </div>
   );
 }
