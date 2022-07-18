21ee8748c9c1d536937bb5dd459eaa98153dc6cc
Michael Law <rmdashrfv@hey.com>
2022-07-18 17:32:54 -0400

    Added menu links

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ function App() {
   return (
     <div className="screen">
       <video autoPlay muted loop id="myVideo">
-        <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mov" />
+        <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mp4" />
       </video>
       <div className="" id="fs-overlay">
       <img
         id="header-img"
         src='https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png'
       />
+      <div className="menu-links">
+        <h3>Create a Character</h3>
+        <h3>About Dungeons & Dragons</h3>
+      </div>
       </div>
     </div>
   );
